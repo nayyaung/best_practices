88f7d3a62aeab6377f63bb4fc8a89b7a086739d0
Nay Yaung <6479987+nayyaung@users.noreply.github.com>
2024-09-10 10:04:54 +0000

    added line for 1st ide

--- example/index.js
+++ example/index.js
@@ -3,4 +3,5 @@ console.log("hello world!");
 console.log("not supposed to run this");
 console.log("added more lines");
 console.log("added fourth line");
+console.log("added line from 1st IDE");
 })();
